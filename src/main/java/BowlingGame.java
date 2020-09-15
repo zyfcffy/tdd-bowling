50c3b40a5ff7933608afbbcf00f2bc591b3030f0
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-15 08:05:17 +0800

    refact code

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,33 +1,30 @@
 public class BowlingGame {
     public int getScore(int[] score) throws Exception {
         int scores = 0;
-        int round = 0;
-        for (int index = 0; index < score.length; index++) {
-            if(score[index] >10||score[index]<0){
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] > 10 || score[i] < 0) {
                 throw new Exception("分数不符合规定");
             }
-            if(score.length<12||score.length>20){
+            if (score.length < 12 || score.length > 21) {
                 throw new Exception("输入的分数不能满足10轮");
             }
             int oneScore = 0;
-            if (score[index] == 10 && index < score.length - 2) {
-                oneScore = 10 + score[index + 1] + score[index + 2];
-                if (index == score.length - 3) {
-                    index += 2;
+            if (score[i] == 10 && i < score.length - 2) {
+                oneScore = 10 + score[i + 1] + score[i + 2];
+                if (i == score.length - 3) {
+                    i += 2;
                 }
-            } else if ((score[index] + score[index + 1] == 10) && index < score.length - 2) {
-                oneScore = 10 + score[index + 2];
-                if (index == score.length - 3) {
-                    index += 1;
+            } else if ((score[i] + score[i + 1] == 10) && i < score.length - 2) {
+                oneScore = 10 + score[i + 2];
+                if (i == score.length - 3) {
+                    i += 1;
                 }
-                index++;
+                i++;
             } else {
-                oneScore = score[index] + score[index + 1];
-                index++;
+                oneScore = score[i] + score[i + 1];
+                i++;
             }
-            round++;
             scores += oneScore;
-
         }
         return scores;
     }
