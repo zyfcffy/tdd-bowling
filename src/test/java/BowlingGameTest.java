e02f18cb957eee5f719f4196cfccf683443cf6c2
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-19 20:17:52 +0800

    [fufangyuan]should throw exception when round more than 10

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -8,16 +8,25 @@ public class BowlingGameTest {
     void should_throw_exception_when_input_illegal() {
         assertThrows(Exception.class, () -> {
             BowlingGame bowlingGame = new BowlingGame();
-            int[] test = {-1,-2, 4, 5, 4, 2, 7, 3, 5, 6, 1, 5, 3, 4, 2, 11, 1};
+            int[] test = {-1, -2, 4, 5, 4, 2, 7, 3, 5, 6, 1, 5, 3, 4, 2, 11, 1};
             int score = bowlingGame.getScore(test);
         });
     }
 
     @Test
-    void should_throw_exception_when_round_not_enough(){
+    void should_throw_exception_when_round_not_enough() {
         assertThrows(Exception.class, () -> {
             BowlingGame bowlingGame = new BowlingGame();
-            int[] test = { 7, 3, 5, 6, 1, 5, 3, 4, 2, 11, 1};
+            int[] test = {7, 3, 5, 6, 1, 5, 3, 4, 2, 11, 1};
+            int score = bowlingGame.getScore(test);
+        });
+    }
+
+    @Test
+    void should_throw_exception_when_round_more_than_10() {
+        assertThrows(Exception.class, () -> {
+            BowlingGame bowlingGame = new BowlingGame();
+            int[] test = {7, 3, 5, 6, 1, 5, 3, 4, 2, 11, 1, 2, 3, 8, 7, 9, 10, 3, 6, 7, 8, 9};
             int score = bowlingGame.getScore(test);
         });
     }
